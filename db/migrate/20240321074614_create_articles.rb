0a1194505a33ba0ca288b164ca957ab3e92df67e
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-21 14:33:46 +0600

    feat: create articles table and model

---
+++ db/migrate/20240321074614_create_articles.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateArticles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+    end
+  end
+end
