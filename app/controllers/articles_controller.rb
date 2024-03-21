6fa3e29ffe697feb06a98c051299ec54e27966bf
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-21 17:34:46 +0600

    feat: create new blog post from form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    if @article.save
+      flash[:notice] = "Article was created successfully!"
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
