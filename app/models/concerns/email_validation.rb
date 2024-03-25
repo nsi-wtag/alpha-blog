406712d6e36d93ea8cd4378ecce6cb9007126b18
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-25 16:25:42 +0600

    feat: add model validations for username and email

---
+++ app/models/concerns/email_validation.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EmailValidator
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'concerns/email_validation'
+
 class User < ApplicationRecord
   has_many :articles
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },
+                    format: { with: EmailValidator::VALID_EMAIL_REGEX }
 end
