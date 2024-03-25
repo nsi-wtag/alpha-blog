e600b0a07eab7008c9db8606c5c9e4793a0b340c
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-25 14:48:32 +0600

    feat: add user id to articles table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 15, maximum: 1000 }
+  belongs_to :user
 end
