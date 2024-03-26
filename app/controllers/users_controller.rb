8ff18f9032ec2885478af9f7ff5fc3f11aab1d32
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-26 15:34:07 +0600

    feat: update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -11,6 +15,17 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = "User updated successfully!"
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
