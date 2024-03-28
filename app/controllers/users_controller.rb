f0f49db56d0a1646aa451d2051e9e975a77b21d1
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-28 14:56:35 +0600

    feat: restrict unauthorized user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @users = User.all
@@ -44,5 +46,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can edit only your account!"
+      redirect_to current_user
+    end
   end
 end
