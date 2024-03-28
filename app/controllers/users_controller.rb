5af2fa5b5e30fb15a2e7aaa49971f721d8f5b0d7
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-28 12:03:58 +0600

    feat: set a user to logged in after successful signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Successfully signed up!"
       redirect_to @user
     else
