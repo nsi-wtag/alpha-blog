bb2098562f65883b139483b307556c195ecf5a30
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-28 16:57:05 +0600

    feat: add admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_25_132348) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_093219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_25_132348) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "articles", "users"
